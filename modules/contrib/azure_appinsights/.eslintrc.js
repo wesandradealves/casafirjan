f145fdb2bba6018c308c8640fb15fe16cca79173
Wesley Andrade <wesandradealves@gmail.com>
2023-10-17 00:04:28 -0300

    .

--- modules/contrib/azure_appinsights/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["eslint-config-drupal-contrib"],
-  globals: {
-    Microsoft: true
-  },
-  root: true
-};
--- modules/contrib/azure_appinsights/js/azai.js
+++
@@ -1,9 +0,0 @@
-(($, drupalSettings) => {
-  const generatedConfig = drupalSettings.azure_appinsights;
-  const init = new Microsoft.ApplicationInsights.ApplicationInsights({
-    config: generatedConfig
-  });
-  const appInsights = init.loadAppInsights();
-
-  appInsights.trackPageView();
-})(jQuery, drupalSettings);
