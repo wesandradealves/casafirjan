4bc4610192bc52d1aa52adfc3259c286ce071df8
Wesley Andrade <wesandradealves@gmail.com>
2023-10-16 23:49:20 -0300

    delete trash

---
+++ sites/default/files/php/twig/652df5f1787c4_block--bannerhome.html.tw_u_AwC6_hmk0d4fdZYcDFPrF2H/trEtfPpkh9zQBJvRNB68a3l-mnzb-Jw1O5GxjbvelDg.php
@@ -0,0 +1,192 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/casa_firjan/templates/blocks/block--bannerhome.html.twig */
+class __TwigTemplate_1dff6a92172d3441f6d7f04816608fbe extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!--
+    Banner Home
+-->
+<div class=\"owl-carousel bannerHome\">
+
+";
+        // line 6
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(twig_get_attribute($this->env, $this->source, ($context["customPage"] ?? null), "bannersHome", [], "any", false, false, true, 6));
+        foreach ($context['_seq'] as $context["_key"] => $context["banner"]) {
+            // line 7
+            echo "    <div target=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["banner"], "link", [], "any", false, false, true, 7), "target", [], "any", false, false, true, 7), 7, $this->source), "html", null, true);
+            echo "\" class=\"item owl-lazy\" data-src=\"";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["banner"], "midia", [], "any", false, false, true, 7), "src", [], "any", false, false, true, 7), 7, $this->source), "html", null, true);
+            echo "\">
+        <div class=\"box  w-100 pb-5 pb-md-0\">
+            <div class=\"container pt-5 pb-5 w-100  d-flex align-items-center justify-content-center text-center\">
+                <div class=\"row m-auto  w-100 d-flex align-items-center justify-content-center text-center\">
+                    <div class=\"col-12 col-lg-6 col-sm-8 m-0 p-0\">
+                    ";
+            // line 12
+            if ( !twig_test_empty(twig_get_attribute($this->env, $this->source, $context["banner"], "texto", [], "any", false, false, true, 12))) {
+                // line 13
+                echo "                        <p class=\"title\">";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["banner"], "titulo", [], "any", false, false, true, 13), 13, $this->source), "html", null, true);
+                echo "</p>
+                        <p class=\"text\">";
+                // line 14
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["banner"], "texto", [], "any", false, false, true, 14), 14, $this->source), "html", null, true);
+                echo "</p>
+                        ";
+                // line 15
+                if (twig_get_attribute($this->env, $this->source, $context["banner"], "url", [], "any", false, false, true, 15)) {
+                    // line 16
+                    echo "                            <a ";
+                    if (twig_get_attribute($this->env, $this->source, $context["banner"], "field_link_background", [], "any", false, false, true, 16)) {
+                        echo " style=\" border: 0; background-color: ";
+                        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["banner"], "field_link_background", [], "any", false, false, true, 16), 16, $this->source), "html", null, true);
+                        echo "; ";
+                        if ((twig_get_attribute($this->env, $this->source, $context["banner"], "field_link_background", [], "any", false, false, true, 16) == "#FFFFFF")) {
+                            echo " color: black; ";
+                        } else {
+                            echo " color: white; ";
+                        }
+                        echo "\" ";
+                    }
+                    echo " target=\"";
+                    ((twig_get_attribute($this->env, $this->source, $context["banner"], "target", [], "any", false, false, true, 16)) ? (print ($this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, ("_" . twig_get_attribute($this->env, $this->source, $context["banner"], "target", [], "any", false, false, true, 16)), "html", null, true))) : (print ("_top")));
+                    echo "\" class=\"btn mt-5\" href=\"";
+                    echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["banner"], "url", [], "any", false, false, true, 16), 16, $this->source), "html", null, true);
+                    echo "\">";
+                    if (twig_get_attribute($this->env, $this->source, $context["banner"], "label", [], "any", false, false, true, 16)) {
+                        echo " ";
+                        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["banner"], "label", [], "any", false, false, true, 16), 16, $this->source), "html", null, true);
+                        echo " ";
+                    } else {
+                        echo " Acesse Aqui ";
+                    }
+                    echo "</a>
+                        ";
+                }
+                // line 18
+                echo "                    ";
+            }
+            // line 19
+            echo "                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['banner'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 25
+        echo "</div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/casa_firjan/templates/blocks/block--bannerhome.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  116 => 25,  105 => 19,  102 => 18,  74 => 16,  72 => 15,  68 => 14,  63 => 13,  61 => 12,  50 => 7,  46 => 6,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!--
+    Banner Home
+-->
+<div class=\"owl-carousel bannerHome\">
+
+{% for banner in customPage.bannersHome %}
+    <div target=\"{{ banner.link.target }}\" class=\"item owl-lazy\" data-src=\"{{ banner.midia.src }}\">
+        <div class=\"box  w-100 pb-5 pb-md-0\">
+            <div class=\"container pt-5 pb-5 w-100  d-flex align-items-center justify-content-center text-center\">
+                <div class=\"row m-auto  w-100 d-flex align-items-center justify-content-center text-center\">
+                    <div class=\"col-12 col-lg-6 col-sm-8 m-0 p-0\">
+                    {% if banner.texto is not empty %}
+                        <p class=\"title\">{{ banner.titulo }}</p>
+                        <p class=\"text\">{{ banner.texto }}</p>
+                        {% if banner.url %}
+                            <a {% if banner.field_link_background %} style=\" border: 0; background-color: {{banner.field_link_background}}; {% if banner.field_link_background == \"#FFFFFF\" %} color: black; {% else %} color: white; {% endif %}\" {% endif %} target=\"{{ banner.target ? '_' ~ banner.target : '_top' }}\" class=\"btn mt-5\" href=\"{{ banner.url }}\">{% if banner.label %} {{banner.label}} {% else %} Acesse Aqui {% endif %}</a>
+                        {% endif %}
+                    {% endif %}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+{% endfor %}
+</div>
+", "themes/casa_firjan/templates/blocks/block--bannerhome.html.twig", "C:\\xampp\\htdocs\\CasaFirjan\\themes\\casa_firjan\\templates\\blocks\\block--bannerhome.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("for" => 6, "if" => 12);
+        static $filters = array("escape" => 7);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for', 'if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
---
+++ sites/default/files/php/twig/652df5f1787c4_block--tagcloud.html.twig_Lke_sF-7VIuG13BFWw7pg6rWf/8PzggkdxFKh4R8keoSJwIjFjBWggZ0buG0uldOCKkbM.php
@@ -0,0 +1,153 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* themes/casa_firjan/templates/blocks/block--tagcloud.html.twig */
+class __TwigTemplate_1b9c3a389ad704313e4a9b15af0a82d3 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        $context["field_background"] = twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (($__internal_compile_0 = ($context["content"] ?? null)) && is_array($__internal_compile_0) || $__internal_compile_0 instanceof ArrayAccess ? ($__internal_compile_0["#block_content"] ?? null) : null), "field_background", [], "any", false, false, true, 1), 0, [], "any", false, false, true, 1), "target_id", [], "any", false, false, true, 1);
+        // line 2
+        echo "
+<section id=\"tagcloud\" class=\"tagcloud mt-5\" style=\"background: url(";
+        // line 3
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->extensions['Drupal\ds_twig_extensions\ApplyPrefix']->_get_image_from_id($this->sandbox->ensureToStringAllowed(($context["field_background"] ?? null), 3, $this->source)), "html", null, true);
+        echo ") center center / cover no-repeat\">
+    <div class=\"container pt-5 pb-5\">
+        <h3>";
+        // line 5
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["title"] ?? null), 5, $this->source), "html", null, true);
+        echo "</h3>
+        ";
+        // line 6
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 6, $this->source), "html", null, true);
+        echo "
+        ";
+        // line 7
+        if (($context["taxonomies"] ?? null)) {
+            // line 8
+            echo "            <ul class=\"taglist p-0 mt-5 d-flex flex-wrap align-items-center justify-content-between\">
+                ";
+            // line 9
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable(($context["taxonomies"] ?? null));
+            foreach ($context['_seq'] as $context["_key"] => $context["taxonomy"]) {
+                // line 10
+                echo "                    <li class=\"pb-4 flex-fill ps-2 pe-2\">
+                        <a class=\"p-2 ps-4 pe-4 tag d-block text-center\" href=\"";
+                // line 11
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["taxonomy"], "url", [], "any", false, false, true, 11), 11, $this->source), "html", null, true);
+                echo "\" data-id=\"";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["taxonomy"], "tid", [], "any", false, false, true, 11), 11, $this->source), "html", null, true);
+                echo "\">";
+                echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["taxonomy"], "name", [], "any", false, false, true, 11), 11, $this->source), "html", null, true);
+                echo "</a>
+                    </li>
+                ";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['taxonomy'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 14
+            echo "            </ul>
+        ";
+        }
+        // line 16
+        echo "    </div>
+</section>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/casa_firjan/templates/blocks/block--tagcloud.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  86 => 16,  82 => 14,  69 => 11,  66 => 10,  62 => 9,  59 => 8,  57 => 7,  53 => 6,  49 => 5,  44 => 3,  41 => 2,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% set field_background = content['#block_content'].field_background.0.target_id %}
+
+<section id=\"tagcloud\" class=\"tagcloud mt-5\" style=\"background: url({{get_image_from_id(field_background)}}) center center / cover no-repeat\">
+    <div class=\"container pt-5 pb-5\">
+        <h3>{{ title }}</h3>
+        {{content}}
+        {% if taxonomies %}
+            <ul class=\"taglist p-0 mt-5 d-flex flex-wrap align-items-center justify-content-between\">
+                {% for taxonomy in taxonomies %}
+                    <li class=\"pb-4 flex-fill ps-2 pe-2\">
+                        <a class=\"p-2 ps-4 pe-4 tag d-block text-center\" href=\"{{taxonomy.url}}\" data-id=\"{{ taxonomy.tid }}\">{{ taxonomy.name }}</a>
+                    </li>
+                {% endfor %}
+            </ul>
+        {% endif %}
+    </div>
+</section>", "themes/casa_firjan/templates/blocks/block--tagcloud.html.twig", "C:\\xampp\\htdocs\\CasaFirjan\\themes\\casa_firjan\\templates\\blocks\\block--tagcloud.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("set" => 1, "if" => 7, "for" => 9);
+        static $filters = array("escape" => 3);
+        static $functions = array("get_image_from_id" => 3);
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'if', 'for'],
+                ['escape'],
+                ['get_image_from_id']
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
